89288e443964300d250c8b14a5043cd3d789f19e
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-01-14 12:10:00 -0500

    added splash screen

--- TypewriterApp/TypewriterApp/ContentView.swift
+++ TypewriterApp/TypewriterApp/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red: 95/255, green: 13/255, blue: 64/255)
+            VStack {            Image("typewriter").resizable().aspectRatio(contentMode: .fit).clipShape(Circle())
+                Text("Hello, World!")
+            }
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
